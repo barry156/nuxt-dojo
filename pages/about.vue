483c9837a7fdc2b486ef5ecde80d9edb59a162cf
Barry Mamadou Siradio <87446001+barry156@users.noreply.github.com>
2025-03-03 20:35:16 +0100

    adding Pages

---
+++ pages/about.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo cumque rerum dolorem, doloremque dolor architecto libero fugit dolore quis! Optio.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo cumque rerum dolorem, doloremque dolor architecto libero fugit dolore quis! Optio.</p>
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style  scoped>
+    h2 {
+        margin-bottom : 20 px;
+        font-size: 36 px;
+    }
+    p {
+        color: #333;
+        margin: 20 px 0;
+    }
+
+</style>
---
+++ pages/products/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>Products</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos reprehenderit at aliquam ab facilis voluptate eius possimus iste commodi nisi?</p>
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style  scoped>
+
+</style>
