e53c4051fe93d2597a570b99f41f1b3708bda8f1
Barry Mamadou Siradio <87446001+barry156@users.noreply.github.com>
2025-03-03 20:42:55 +0100

    Route Parameters

---
+++ pages/products/[id].vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p>Product details fot {{ id }}</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, cumque dolore! Dicta ipsa, expedita eos vitae fugiat iure distinctio! Sapiente.</p>
+
+    </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
